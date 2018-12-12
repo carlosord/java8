63c57be97bec9fbd5b16af94f9619197c850c667
RICOH-EUROPE\agustin.arboleya <agustin.arboleya@ricoh.es>
2018-12-12 18:25:01 +0100

    stream map examples

--- src/main/java/com/example/java8/stream/StreamMap.java
+++ src/main/java/com/example/java8/stream/StreamMap.java
@@ -1,5 +1,29 @@
 package com.example.java8.stream;
 
-public class StreamMap {
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Collectors;
 
+import com.example.java8.model.Person;
+
+public class StreamMap {
+	/** The Constant FILE. */
+	private static final String FILE = "src/main/resources/data.csv";
+
+	public static void main(String[] args) throws IOException {
+		
+		List<Person> people = Files.lines(Paths.get(FILE)).map(line -> { 
+			String[] splitedLine = line.split(";");
+			return new Person(splitedLine[0], splitedLine[1], splitedLine[2], splitedLine[3]);
+		}).collect(Collectors.toList());
+		
+		System.out.println("Concat emails");
+		System.out.println(people.stream().map(p -> p.getMail()).reduce("", (a,b) -> a + ", " + b));
+		
+		System.out.println("Upper names");
+		people.stream().filter(p -> p.getName().startsWith("J")).map(p -> p.getName().toUpperCase()).forEach(System.out::println);
+		
+	}
 }
