1b80bb0543a6fcce0324be336a6d7d7fbbb0af92
agustin.arboleya <agustin.arboleya@ricoh.es>
2018-12-14 08:59:02 +0100

    Option example

---
+++ src/main/java/com/example/java8/option/OptionExample.java
@@ -0,0 +1,12 @@
+package com.example.java8.option;
+
+import java.io.IOException;
+import java.util.Optional;
+
+import com.example.java8.model.Person;
+
+public class OptionExample {
+	public static void main(String[] args) throws IOException {
+		Optional<Person> emptyPerson = Optional.empty();
+	}
+}
